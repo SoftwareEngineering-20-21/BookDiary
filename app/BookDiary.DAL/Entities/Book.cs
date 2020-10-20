a6f40e83df502d794ed8844079b35d90d4ed8eff
Alina Naiborodina <alina26158@gmail.com>
2020-10-20 21:53:49 +0300

    Add "Book" entity

---
+++ app/BookDiary.DAL/Entities/Book.cs
@@ -0,0 +1,34 @@
+﻿using BookDiary.DAL.Abstractions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookDiary.DAL.Entities
+{
+    public enum BookStatus
+    {
+        InProgress,
+        Completed,
+        Planned
+    }
+    public class Book : AbstractEntity
+    {
+        
+
+        public BookStatus Status { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(225)]
+        public string Title { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(225)]
+        public string Author { get; set; }
+
+        [Required]
+        public int TotalPages { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ app/BookDiary.DAL/Migrations/202010201846193_CreateBooksTable.cs
@@ -0,0 +1,30 @@
+﻿namespace BookDiary.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateBooksTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Status = c.Int(nullable: false),
+                        Title = c.String(nullable: false, maxLength: 225, unicode: false),
+                        Author = c.String(nullable: false, maxLength: 225, unicode: false),
+                        TotalPages = c.Int(nullable: false),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Books");
+        }
+    }
+}
