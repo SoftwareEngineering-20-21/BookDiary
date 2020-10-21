336b699bf4992e7542c41dcb0c0b1a0830dd6f47
YuliaYur <40237840+YuliaYur@users.noreply.github.com>
2020-10-21 13:26:35 +0300

    Add Statistic Entity

---
+++ app/BookDiary.DAL/Entities/Statistic.cs
@@ -0,0 +1,26 @@
+﻿using BookDiary.DAL.Abstractions;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookDiary.DAL.Entities
+{
+    public class Statistic : AbstractEntity
+    {
+        [Required]
+        public DateTimeOffset Day { get; set; }
+
+        [Required]
+        public int OldPages { get; set; }
+
+        [Required]
+        public int NewPages { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [Required]
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+    }
+}
