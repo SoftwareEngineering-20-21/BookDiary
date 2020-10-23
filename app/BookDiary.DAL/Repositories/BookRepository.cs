4e433e263fdd214868fe2bb139fd52e850f0481b
Alina Naiborodina <alina26158@gmail.com>
2020-10-23 17:11:26 +0300

    Create book repository

---
+++ app/BookDiary.DAL/Repositories/BookRepository.cs
@@ -0,0 +1,53 @@
+﻿using BookDiary.DAL.EF;
+using BookDiary.DAL.Entities;
+using BookDiary.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace BookDiary.DAL.Repositories
+{
+    class BookRepository : IRepository<Book>
+    {
+        private AppDbContext db;
+
+        public BookRepository(AppDbContext context)
+        {
+            this.db = context;
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return db.Books;
+        }
+
+        public Book Get(int id)
+        {
+            return db.Books.Find(id);
+        }
+
+        public void Create(Book book)
+        {
+            db.Books.Add(book);
+        }
+
+        public void Update(Book book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+        }
+
+        public IEnumerable<Book> Find(Func<Book, Boolean> predicate)
+        {
+            return db.Books.Where(predicate).ToList();
+        }
+
+        public void Delete(int id)
+        {
+            Book book = db.Books.Find(id);
+            if (book != null)
+                db.Books.Remove(book);
+        }
+    }
+}
