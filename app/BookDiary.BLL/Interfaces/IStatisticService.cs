87f671b324d12fec7602f161cd1b39f5c7076d46
Alina Naiborodina <alina26158@gmail.com>
2020-11-20 13:36:43 +0200

    create DTO statictis

---
+++ app/BookDiary.BLL/Interfaces/IStatisticService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BookDiary.BLL.DTO;
+
+namespace BookDiary.BLL.Interfaces
+{
+    class IStatisticService
+    {
+        void CreateStatistic(StatisticDTO statisticDTO);
+        void UpdateBook(BookDTO bookDTO);
+        void DeleteBook(BookDTO bookDTO);
+        BookDTO GetBook(int? id);
+        IEnumerable<BookDTO> GetBooks();
+        IEnumerable<BookDTO> GetBooksByUserId(int? userId);
+    }
+}
