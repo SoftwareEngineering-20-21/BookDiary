718fdf171ffde7580f45aad1250dea95ad22f1de
Alina <alina26158@gmail.com>
2020-12-15 19:16:57 +0200

    Add statistic to editing book

--- app/BookDiary.PL/views/BookPage.xaml.cs
+++ app/BookDiary.PL/views/BookPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using BookDiary.BLL.DTO;
 using BookDiary.BLL.Interfaces;
@@ -14,18 +16,40 @@ namespace BookDiary.PL
     {
         private BookDTO book;
 
+        private StatisticDTO statistic;
+
         private IKernel container;
 
         private IBookService bookService;
+
+        private IStatisticService statisticService;
         public BookPage(IKernel container, BookDTO book)
         {
             this.container = container;
             this.bookService = container.Get<IBookService>();
+            statisticService = container.Get<IStatisticService>();
+            List<StatisticDTO> statistics = statisticService.GetStatistics().Where(x => x.BookId == book.Id && x.Day.Date == DateTimeOffset.Now.Date).ToList();
+            if (statistics.Count == 0)
+            {
+                statisticService.CreateStatistic(new StatisticDTO
+                {
+                    Day = DateTimeOffset.Now.Date,
+                    OldPages = 0,
+                    NewPages = 0
+            });
+            }
+            statistics = statisticService.GetStatistics().Where(x => x.BookId == book.Id && x.Day.Date == DateTimeOffset.Now.Date).ToList();
+            statistic = statistics.ElementAt(0);
+            
             InitializeComponent();
 
             title.Text = book.Title;
             totalPages.Text = Convert.ToString(book.TotalPages);
             author.Text = book.Author;
+            readPages.Text = book.ReadPages.ToString();
+            todayReadPages.Text = statistic.NewPages.ToString();
+            Mark.Text = book.Mark.ToString();
+            Review.Text = book.Review;
 
             this.book = book;
         }
@@ -60,7 +84,11 @@ namespace BookDiary.PL
             book.Mark = Convert.ToInt32(Mark.Text);
             book.Review = Review.Text;
 
+            statistic.NewPages = Convert.ToInt32(todayReadPages.Text);
+            statistic.Day = DateTimeOffset.Now.Date;
+            statisticService.UpdateStatistic(statistic);
             bookService.UpdateBook(book);
+            this.Hide();
         }
         private void NotificationBook_Click(object sender, RoutedEventArgs e)
         {
