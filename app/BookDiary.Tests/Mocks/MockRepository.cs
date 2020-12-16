f072e143cd0e6cdae11cfe6189d7f85087eea1f9
Alexandr Shkurinskiy <ashkurinskiy@gmail.com>
2020-12-17 01:27:10 +0200

    Add UserService unit tests

---
+++ app/BookDiary.Tests/Mocks/MockRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookDiary.DAL.Interfaces;
+using BookDiary.DAL.Abstractions;
+
+namespace BookDiary.Tests.Mocks
+{
+    public class MockRepository<T> : IRepository<T> where T : AbstractEntity
+    {
+        List<T> Items;
+
+        public MockRepository()
+        {
+            this.Items = new List<T>();
+        }
+        public IEnumerable<T> Get()
+        {
+            return Items;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Items;
+        }
+
+        public T Get(int id)
+        {
+            return Items.Find(x => x.Id == id);
+        }
+
+        public IEnumerable<T> Get(Func<T, bool> predicate)
+        {
+            return Items.Where(predicate);
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            return Items.Where(predicate);
+        }
+
+        public void Create(T item)
+        {
+            Items.Add(item);
+        }
+
+        public void Update(T item) 
+        {
+            var index= Items.FindIndex(x => x.Id == item.Id);
+            Items[index] = item;
+        }
+
+        public void Delete(int id)
+        {
+            var index = Items.FindIndex(x => x.Id == id);
+            Items.RemoveAt(index);
+        }
+
+        public void Save() { }
+
+        public Task SaveAsync() { return Task.FromResult(0); }
+    }
+}
---
+++ app/BookDiary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BookDiary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookDiary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c016e2d-4fa1-4b45-bdcb-8f31e3a573a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
